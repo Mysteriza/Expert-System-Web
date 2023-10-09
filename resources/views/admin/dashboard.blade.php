6fcd6a804c6acd44bd45ba491ed19c27901e4535
Mysteriza <rifqifachriza24@gmail.com>
2023-10-09 15:20:57 +0700

    first commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,187 @@
+@extends('admin.admin_main')
+@section('title', 'Dashboard')
+
+{{-- isi --}}
+@section('admin_content')
+    <!-- Page content-->
+    <main id="main" class="main">
+
+        <div class="pagetitle">
+          <h1>Dashboard</h1>
+          <nav>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+          </nav>
+        </div><!-- End Page Title -->
+
+        <section class="section dashboard">
+          <div class="row">
+
+            <!-- Left side columns -->
+            <div class="col-lg-12">
+              <div class="row">
+
+                <!-- Sales Card -->
+                <div class="col-xxl-4 col-md-6">
+                  <div class="card info-card sales-card">
+
+                    <div class="filter">
+                      <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                      <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                        <li class="dropdown-header text-start">
+                          <h6>Filter</h6>
+                        </li>
+                    </div>
+
+                    <div class="card-body">
+                      <h5 class="card-title">Daftar <span>| Indikator</span></h5>
+
+                      <div class="d-flex align-items-center">
+                        <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                          <i class="bi bi-activity"></i>
+                        </div>
+                        <div class="ps-3">
+                          <h6>{{$gejala->count()}}</h6>
+                          <span class="text-success small pt-1 fw-bold">{{$gejala->count()}}</span> <span class="text-muted small pt-2 ps-1">indikator</span>
+
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
+                </div><!-- End Sales Card -->
+
+                <!-- Customers Card -->
+                <div class="col-xxl-4 col-xl-12">
+
+                  <div class="card info-card customers-card">
+
+                    <div class="filter">
+                      <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                      <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                        <li class="dropdown-header text-start">
+                          <h6>Filter</h6>
+                        </li>
+                      </ul>
+                    </div>
+
+                    <div class="card-body">
+                      <h5 class="card-title">Jumlah <span>| Admin</span></h5>
+
+                      <div class="d-flex align-items-center">
+                        <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                          <i class="bi bi-people"></i>
+                        </div>
+                        <div class="ps-3">
+                          <h6>{{$user->count()}}</h6>
+                          <span class="text-danger small pt-1 fw-bold">{{$user->count()}}</span> <span class="text-muted small pt-2 ps-1">admin</span>
+
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+
+                </div><!-- End Customers Card -->
+
+
+
+                <!-- Recent Sales -->
+                <div class="col-12">
+                  <div class="card recent-sales overflow-auto">
+
+                    <div class="filter">
+                      <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                      <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                        <li class="dropdown-header text-start">
+                          <h6>Filter</h6>
+                        </li>
+                      </ul>
+                    </div>
+
+                    <div class="card-body">
+                      <h5 class="card-title">Daftar <span>| Indikator</span></h5>
+
+                      <table class="table table-borderless datatable">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Kode Indikator</th>
+                            <th scope="col">Indikator</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($gejala as $item)
+                          <tr>
+                            <th scope="row"><a href="#">#{{ $loop->iteration }}</a></th>
+                            <td><a href="#" class="text-primary">{{ $item->kode_gejala }}</a></td>
+                            <td>{{ $item->gejala }}</td>
+                          </tr>
+                          @endforeach
+                        </tbody>
+                      </table>
+
+                    </div>
+
+                  </div>
+                </div><!-- End Recent Sales -->
+
+                <!-- Top Selling -->
+                <div class="col-12">
+                  <div class="card top-selling overflow-auto">
+
+                    <div class="filter">
+                      <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                      <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                        <li class="dropdown-header text-start">
+                          <h6>Filter</h6>
+                        </li>
+
+                        <li><a class="dropdown-item" href="#">Today</a></li>
+                        <li><a class="dropdown-item" href="#">This Month</a></li>
+                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                      </ul>
+                    </div>
+
+                    <div class="card-body pb-0">
+                      <h5 class="card-title">Gangguan <span>| Depresi</span></h5>
+
+                      <table class="table table-borderless">
+                        <thead>
+                          <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Kode depresi</th>
+                            <th scope="col">Tingkat depresi</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($tingkat_depresi as $item)
+                          <tr>
+                            <th scope="row"><a href="#">#{{ $loop->iteration }}</a></th>
+                            <td><a href="#" class="text-primary">{{ $item->kode_depresi }}</a></td>
+                            <td>{{ $item->depresi }}</td>
+                          </tr>
+                          @endforeach
+
+                        </tbody>
+                      </table>
+
+                    </div>
+
+                  </div>
+                </div><!-- End Top Selling -->
+
+              </div>
+            </div><!-- End Left side columns -->
+
+
+
+          </div>
+        </section>
+
+      </main><!-- End #main -->
+
+
+@endsection
---
+++ resources/views/landing.blade.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]>
+<!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+
+    <!-- Basic Page Needs
+ ================================================== -->
+    <meta charset="utf-8">
+    <title>Menentukan Bakat Anak</title>
+    <meta name="description" content="Professional Creative Template" />
+    <meta name="author" content="IG Design">
+    <meta name="keywords"
+        content="ig design, website, design, html5, css3, jquery, creative, clean, animated, portfolio, blog, one-page, multi-page, corporate, business," />
+    <meta property="og:title" content="Professional Creative Template" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="" />
+    <meta property="og:image" content="" />
+    <meta property="og:image:width" content="470" />
+    <meta property="og:image:height" content="246" />
+    <meta property="og:site_name" content="" />
+    <meta property="og:description" content="Professional Creative Template" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="https://twitter.com/IvanGrozdic" />
+    <meta name="twitter:domain" content="http://ivang-design.com/" />
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:description" content="Professional Creative Template" />
+    <meta name="twitter:image" content="http://ivang-design.com/" />
+
+    <!-- Mobile Specific Metas
+ ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="theme-color" content="#212121" />
+    <meta name="msapplication-navbutton-color" content="#212121" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="#212121" />
+
+    <!-- Web Fonts
+ ================================================== -->
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900"
+        rel="stylesheet" />
+    <link
+        href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet" />
+    <!-- CSS
+ ================================================== -->
+    <link rel="stylesheet" href="landing/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="landing/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="landing/css/style.css" />
+    <link rel="stylesheet" href="landing/css/colors/color.css" />
+    <link rel="stylesheet" href="landing/css/ionicons.min.css" />
+    <link rel="stylesheet" href="landing/css/owl.carousel.css" />
+    <link rel="stylesheet" href="landing/css/owl.transitions.css" />
+
+    <!-- Favicons
+ ================================================== -->
+    <link rel="icon" type="image/png" href="favicon.png">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
+
+
+</head>
+
+<body class="royal_preloader">
+
+    <!-- <div id="royal_preloader"></div> -->
+
+
+
+    <!-- Nav and Logo
+ ================================================== -->
+
+
+    <!-- MENU
+    ================================================== -->
+
+    <nav id="menu-wrap" class="menu-back cbp-af-header">
+        <div class="menu">
+            <a href="/">
+                <div class="logo"></div>
+            </a>
+            <ul>
+
+                @guest()
+                <li>
+                    <a class="shadow-hover" href="/login">Login</span></a>
+                </li>
+                @endguest
+            </ul>
+        </div>
+    </nav>
+
+    <!-- Primary Page Layout
+ ================================================== -->
+
+    <main>
+
+        <!-- Hero Section -->
+
+        <div class="section full-height mob-height">
+            <div class="background-parallax" style="background-image: url('landing/img/full-2.jpg')"
+                data-enllax-ratio=".5" data-enllax-type="background" data-enllax-direction="vertical"></div>
+            <div class="hero-center-text-wrap">
+                <div class="container text-left">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <br>
+                            <br>
+                            <h1 class="parallax-fade-top-2 typed">Cek Bakat <strong>Anak</strong>
+                                Sekarang!<br><span id="typed-1"></span></h1>
+                            <br>
+                            <style>
+                                .btn-glow:hover {
+                                  box-shadow: 0 0 10px rgba(255, 255, 255, 0.5);
+                                  transition: box-shadow 0.5s;
+                                }
+                              </style>
+                              <div style="margin-left: 12px">
+
+                                <a href="/form-faq"  class="btn btn-dark btn-glow" role="button" style="color: rgb(218, 116, 22);">
+                                  Tentukan Bakat Anak
+                                </a>
+                              </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a href="#top-scroll" data-gal="m_PageScroll2id">
+                <div class="scroll-to-next">ke bawah <i class="fa fa-long-arrow-down"></i></div>
+            </a>
+        </div>
+
+        <!-- Portfolio -->
+
+
+        <!-- Hero Section -->
+
+        <div class="section padding-top-big padding-bottom-big">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 page-center-text-wrap text-center">
+                        <h1 class="parallax"><strong>-</strong> Project akhir semester mata kuliah
+                            <strong>-</strong><br><span>Sistem Pakar</span></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section padding-top-bottom-big background-dark">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 text-center">
+                        <div id="owl-sep-1" class="owl-carousel owl-theme">
+                            <div class="item">
+                                <div class="quote">
+                                    <p class="lead">"Temukan Potensi Sejati Anak Anda: Mengungkap Bakat dan Mengarahkan Masa Depan Mereka."</p>
+                                    <h6>
+                                        <h6>&rarr;</h6>
+                                    </h6>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="quote">
+                                    <p class="lead">"Masa depan yang sukses dimulai dengan mengenali dan mengasah bakat anak-anak kita. Kami hadir untuk mempersembahkan suatu perjalanan penemuan diri yang menarik dan bermanfaat, di mana Anda dapat menggali potensi anak-anak Anda secara holistik. Melalui rangkaian tes dan evaluasi, kami akan membantu Anda mengidentifikasi dan mengarahkan bakat-bakat unik mereka, memperkuat kepercayaan diri mereka, serta memberikan dukungan dan sumber daya yang diperlukan untuk mencapai keberhasilan dan kebahagiaan sejati dalam hidup mereka."</p>
+                                    <h6>&rarr;</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section padding-top-big">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-5 text-center mb-5">
+                        <h4>Kelompok X</h4>
+                        <p class="lead">Sistem Pakar</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center">
+                        <div class="team-wrap">
+                            <img src="landing/img/team2.jpg" alt="">
+                            <p>15-2020-052</p>
+                            <h6>Muhammad Fiqri Ramadhan</h6>
+                        </div>
+                    </div>
+                    <div class="col mt-4 m-xl-0 text-center">
+                        <div class="team-wrap">
+                            <img src="landing/img/team4.jpg" alt="">
+                            <p>15-2020-079</p>
+                            <h6>Regy Putra Adithia</h6>
+                        </div>
+                    </div>
+                    <div class="col mt-4 m-md-0 text-center">
+                        <div class="team-wrap">
+                            <img src="landing/img/team3.jpg" alt="">
+                            <p>15-2020-082</p>
+                            <h6>Rifqi Fachriza Sukmana</h6>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+
+        <div class="section padding-top-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="full-line"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+    <!-- JAVASCRIPT
+    ================================================== -->
+    <script src="landing/js/jquery.js"></script>
+    <script src="landing/js/royal_preloader.min.js"></script>
+    <script src="landing/js/popper.min.js"></script>
+    <script src="landing/js/bootstrap.min.js"></script>
+    <script src="landing/js/plugins.js"></script>
+    <script src="landing/js/custom.js"></script>
+    <script>
+        // Type text
+
+        var typed = new Typed('#typed-1', {
+            strings: ['diagnosa', 'pahami', 'kembangkan'],
+            typeSpeed: 45,
+            backSpeed: 0,
+            startDelay: 200,
+            backDelay: 2200,
+            loop: true,
+            loopCount: false,
+            showCursor: true,
+            cursorChar: "_",
+            attr: null
+        });
+    </script>
+    <!-- End Document
+================================================== -->
+</body>
+
+</html>
